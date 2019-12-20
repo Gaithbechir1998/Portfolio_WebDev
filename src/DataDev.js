ca6839f0e64821268ca18c237db7957d9561b65f
Gaithbechir1998 <gaithbechir1998@gmail.com>
2019-12-20 17:55:18 +0100

    add mors details for file DataDev

---
+++ src/DataDev.js
@@ -0,0 +1,224 @@
+let DataDev = {
+    "imagebaseurl":"https://gaithbechir1998.gitlab.io/web-front-end/",
+    "name": "Gaith Bechir",
+    "role": "Frontend Developer and Backend",
+    "gmailaccount":" gaithbechir1998@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I am a good team player with knowledge in the web development and background of software development, I also have a strong feeling of responsibility. In addition, I am open minded and a quick learner.",
+    "socialLinks":[
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/Gaithbechi",
+          "className":"fa fa-facebook"
+        },
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/gaith-bechir-074407171/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Gaithbechir1998",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"future ingénieur informatique 💻. I am a good team player with knowledge in the web development and background of software development, I also have a strong feeling of responsibility. In addition, I am open minded and a quick learner.",
+    "address":"Tunis",
+    "website":"https://gaithbechir1998.gitlab.io/web-front-end/",
+    "education":[
+      {
+        "UniversityName":"Lycée secondaire de jelma - Sidi Bouzid",
+        "specialization":"Student",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2017",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Lycée zahra sousse",
+        "specialization":"Student",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Institut Supérieur d'Informatique et de Gestion de Kairouan",
+        "specialization":"Student",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2019",
+        "Achievements":"spécialité informatique"
+      },
+      {
+        "UniversityName":"Institut Supérieur des Sciences Appliquées et de Technologie de Sousse",
+        "specialization":"Student",
+        "MonthOfPassing":"...",
+        "YearOfPassing":"...",
+        "Achievements":"spécialité informatique"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Freelance",
+        "specialization":"cybersecurity",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2013~2014",
+        "Achievements":"kali linux, metasploit, +70 Tools In kali linux ..."
+      },
+      {
+        "CompanyName":"Freelance",
+        "specialization":"Frontend",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2015~2017",
+        "Achievements":"HTML, Css, Bootstrap, Javascript"
+      },
+      {
+        "CompanyName":"Freelance",
+        "specialization":"Backend",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2016~2017",
+        "Achievements":"PHP, MySQL"
+      },
+      {
+        "CompanyName":"Freelance",
+        "specialization":"Programming",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2017~2019",
+        "Achievements":"Pascal, C, C++, Java, Bash, Ruby, Python..."
+      },
+      {
+        "CompanyName":"Freelance",
+        "specialization":"FrameWork Javascript",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018~2019",
+        "Achievements":"Js, Nodejs, Reactjs, MongoDB"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"bootstrap"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"linux",
+        "description":"linux debian brand logo spiral",
+        "imgurl":"https://images.wallpaperscraft.com/image/linux_debian_brand_logo_spiral_93908_1280x960.jpg"
+      },
+      {
+        "name":"ipad",
+        "description":"ipad iphone pens notepads coffee starbucks",
+        "imgurl":"https://images.wallpaperscraft.com/image/ipad_iphone_pens_notepads_coffee_starbucks_83901_1280x960.jpg"
+      },
+      {
+        "name":"asus",
+        "description":"asus logo republic of gamers computer",
+        "imgurl":"https://images.wallpaperscraft.com/image/asus_logo_republic_of_gamers_computer_97501_1280x960.jpg"
+      },
+      {
+        "name":"plate",
+        "description":"plate sensor windows android",
+        "imgurl":"https://images.wallpaperscraft.com/image/plate_sensor_windows_android_97168_1280x960.jpg"
+      },
+      {
+        "name":"code",
+        "description":"code programming text",
+        "imgurl":"https://images.wallpaperscraft.com/image/code_programming_text_140050_1280x960.jpg"
+      },
+      {
+        "name":"code",
+        "description":"code programming text",
+        "imgurl":"https://images.wallpaperscraft.com/image/code_programming_text_141301_1280x960.jpg"
+      },
+      {
+        "name":"programming",
+        "description":"code lines programming",
+        "imgurl":"https://images.wallpaperscraft.com/image/code_lines_programming_130775_1280x960.jpg"
+      },
+      {
+        "name":"colorful",
+        "description":"code text colorful",
+        "imgurl":"https://images.wallpaperscraft.com/image/code_text_colorful_140555_1280x960.jpg"
+      },
+      {
+        "name":"code",
+        "description":"code letters screen",
+        "imgurl":"https://images.wallpaperscraft.com/image/code_letters_screen_137590_1280x960.jpg"
+      },
+      {
+        "name":"ipad",
+        "description":"imac ipad apple",
+        "imgurl":"https://images.wallpaperscraft.com/image/imac_ipad_apple_106481_1280x960.jpg"
+      },
+      {
+        "name":"smartphone",
+        "description":"htc one m8 smartphone touchscreen",
+        "imgurl":"https://images.wallpaperscraft.com/image/htc_one_m8_smartphone_touchscreen_101415_1280x960.jpg"
+      },
+      {
+        "name":"iphone",
+        "description":"iphone apple headphones display",
+        "imgurl":"https://images.wallpaperscraft.com/image/iphone_6_apple_headphones_display_106208_1280x960.jpg"
+      },
+      {
+        "name":"matrix",
+        "description":"matrix code numbers",
+        "imgurl":"https://images.wallpaperscraft.com/image/matrix_code_numbers_147523_1280x960.jpg"
+      },
+      {
+        "name":"apple",
+        "description":"apple brand imac desk keyboard",
+        "imgurl":"https://images.wallpaperscraft.com/image/apple_brand_imac_desk_keyboard_79998_1280x960.jpg"
+      },
+      {
+        "name":"samsung",
+        "description":"samsung galaxy note phone style display touch screen",
+        "imgurl":"https://images.wallpaperscraft.com/image/samsung_galaxy_note_phone_style_display_touch_screen_83897_1280x960.jpg"
+      },
+      {
+        "name":"Code",
+        "description":"code programming symbols",
+        "imgurl":"https://images.wallpaperscraft.com/image/code_programming_symbols_147674_1280x960.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Frontend Web Dev",
+        "name":"HTML"
+      },
+      {
+        "description":"Frontend Web Dev",
+        "name":"Css"
+      },
+      {
+        "description":"Frontend Web Dev",
+        "name":"Bootstrap"
+      },
+      {
+        "description":"Frontend Web Dev",
+        "name":"Javascript"
+      },
+      {
+        "description":"Frontend Web Dev",
+        "name":"ReactJs"
+      },
+      {
+        "description":"Backend Web Dev",
+        "name":"Nodejs"
+      },
+      {
+        "description":"Backend Web Dev",
+        "name":"Mongodb"
+      }
+    ]
+  }
+  
+  export default DataDev
