72331f33bd9e46ee3afea93ad878066211c72b79
Gaithbechir1998 <gaithbechir1998@gmail.com>
2019-12-20 22:35:08 +0100

    Page Resume modifier and add all props and state for my page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,100 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+export default  class Resume extends Component {
+  render() {
+    let GaithData = this.props.GaithData;
+    return (
+      <section id="resume">
 
-export default class Resume extends Component {
-    render() {
-        return (
-            <div>
-                Resume
+         <div className="row education">
+
+            <div className="three columns header-col">
+               <h1><span>Education</span></h1>
             </div>
-        )
-    }
+
+            <div className="nine columns main-col">
+              {
+                GaithData.education && GaithData.education.map((item)=>{
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.UniversityName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
+                          <p>
+                          {item.Achievements}
+                          </p>
+                       </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+         </div>
+        <div className="row work">
+            <div className="three columns header-col">
+               <h1><span>Work</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                GaithData.work && GaithData.work.map((item) => {
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.CompanyName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
+                          <p>
+                          {item.Achievements}
+                          </p>
+                       </div>
+
+                    </div>
+
+                  )
+                })
+              }
+            </div> 
+         </div>
+
+
+         <div className="row skill">
+
+            <div className="three columns header-col">
+               <h1><span>Skills</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+
+               <p>
+               {GaithData.skillsDescription}
+               </p>
+
+   				<div className="bars">
+
+   				   <ul className="skills">
+                {
+                  GaithData.skills && GaithData.skills.map((item) => {
+                    return(
+                      <li>
+                      <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
+                      </span><em>{item.skillname}</em>
+                      </li>
+                    )
+                  })
+                }
+
+   					</ul>
+
+   				</div>
+
+   			</div>
+
+         </div>
+
+      </section>
+    );
+  }
 }
