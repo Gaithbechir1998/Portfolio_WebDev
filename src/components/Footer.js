a784a65b6b13c434a7e6086bcee9f5b40f815165
Gaithbechir1998 <gaithbechir1998@gmail.com>
2019-12-20 22:30:45 +0100

    Page Footer modifier and add all props and state for my page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
 export default class Footer extends Component {
-    render() {
-        return (
-            <div>
-                Footer
-            </div>
-        )
-    }
+  render() {
+    let DataDev = this.props.DataDev;
+    return (
+      <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">
+            {
+              DataDev.socialLinks && DataDev.socialLinks.map((item)=>{
+                return(
+                  <li>
+                    <a href={item.url}>
+                    <i className={item.className} />
+                    </a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+          
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+      </div>
+    </footer>
+    );
+  }
 }
