cfd255f50cf78fd22b7abbf71e9e1f3c052db2e0
Gaithbechir1998 <gaithbechir1998@gmail.com>
2019-12-20 22:21:21 +0100

    Page About modifier and add all props and state for my page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,45 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
 export default class About extends Component {
-    render() {
-        return (
-            <div>
-                About
+  render() {
+    let DataDev = this.props.DataDev;
+    return (
+      <section id="about">
+         <div className="row">
+
+            <div className="three columns">
+
+               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
+
             </div>
-        )
-    }
+
+            <div className="nine columns main-col">
+
+               <h2>About Me</h2>
+               <p>
+               {
+                 DataDev.aboutme
+               }
+               </p>
+
+               <div className="row">
+
+                  <div className="columns contact-details">
+
+                  <h2>Contact Details</h2>
+                  <p className="address">
+       						<span>{DataDev.name}</span>
+                     <br></br>
+       						   <span>
+                     {DataDev.address}
+                    </span>
+                    <br></br>
+                    <span>{DataDev.website}</span>
+       					   </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+    );
+  }
 }
