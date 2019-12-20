d0c9fa9bcaecf77969ab91e2066733f51feedbbc
Gaithbechir1998 <gaithbechir1998@gmail.com>
2019-12-20 22:41:36 +0100

    Page Testimonials modifier and add all props and state for my page

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,11 +1,37 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
 export default class Testimonials extends Component {
-    render() {
-        return (
-            <div>
-                Testmonials
+  render() {
+    let GaithData = this.props.GaithData;
+    return (
+      <section id="testimonials">
+        <div className="text-container">
+          <div className="row">
+            <div className="two columns header-col">
+              <h1><span>Client Testimonials</span></h1>
             </div>
-        )
-    }
+            <div className="ten columns flex-container">
+              <div className="flexslider">
+                <ul className="slides">
+                  {
+                    GaithData.testimonials && GaithData.testimonials.map((item)=>{
+                      return(
+                        <li>
+                          <blockquote>
+                            <p>
+                            {item.description}
+                            </p>
+                            <cite>{item.name}</cite>
+                          </blockquote>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </div> {/* div.flexslider ends */}
+            </div> {/* div.flex-container ends */}
+          </div> {/* row ends */}
+        </div>  {/* text-container ends */}
+      </section>
+        );
+  }
 }
