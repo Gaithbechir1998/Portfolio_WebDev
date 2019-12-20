32997060863376c781ecc6862c86a6002268332d
Gaithbechir1998 <gaithbechir1998@gmail.com>
2019-12-20 22:01:51 +0100

    Import All Components In Principal App and declare Props In App src/App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-        
-        Hello Word !!
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Testimonials from  './components/Testimonials';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import DataDev from './DataDev';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header DataDev={DataDev}/>
+        <About DataDev={DataDev}/>
+        <Resume DataDev={DataDev}/>
+        <Portfolio DataDev={DataDev}/>
+        <Testimonials DataDev={DataDev}/>
+        <ContactUs DataDev={DataDev}/>
+        <Footer DataDev={DataDev}/>
+      </div>
+    );
+  }
 }
 
 export default App;
